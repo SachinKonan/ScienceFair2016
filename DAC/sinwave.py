b7b742e8196cfa3dc98070bbf3eec3c8a635103b
SachinKonan <sachinkonan480@gmail.com>
2016-11-13 17:02:55 +0000

    code

--- DAC/sinwave.py
+++ DAC/sinwave.py
@@ -4,10 +4,12 @@ from threading import Thread
 import Adafruit_MCP4725
 
 class dacThreadSin:
-    def __init__(self,address):
+    def __init__(self,address,frequency = 1):
         self.i2caddress = address
         self.interrupt = False
         self.dac = Adafruit_MCP4725.MCP4725(self.i2caddress)
+        self.degree = 0
+        self.frequency = frequency
         
     def start(self):
         print('Starting DAC Thread')
@@ -19,13 +21,23 @@ class dacThreadSin:
             if(self.interrupt):
                 return
             
-            for i in range(0, 360):
-                radians = i * np.pi/180
-                val = np.sin(radians) * 2048 + 2048
-                #print(round(val))
-                self.dac.set_voltage(int(round(val)),True)
-                time.sleep(0.01)
+            else:
+				radians = (self.degree * np.pi/180)
+				val = np.sin(2 * np.pi * self.frequency* np.radians(self.degree)) * 2048 + 2048
+				#print(val)
+				self.dac.set_voltage(int(round(val)),True)
+				self.checkdegree()
+				time.sleep(0.1)
     
+    def checkdegree(self):
+		"""
+		if(self.degree > 360):
+			self.degree = 0
+		else:
+			self.degree = self.degree
+			"""
+		self.degree += 1
+		
     def stop(self):
         self.interrupt = True
 
